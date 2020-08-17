d3a5b5f6d3c90faebdd886a3edfe3dc8579762e7
gusfrehse <gusfrehse@gmail.com>
2020-08-17 15:42:08 -0300

    Added the trap codes, memory mapped registers, mem_read, mem_write and theoretically finished

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <fstream>
 #include <cstdint>
+
+#include <signal.h>
+/* unix */
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 uint16_t sign_extend(uint16_t x, int bit_count);
 void update_flags(uint16_t r);
@@ -8,6 +19,7 @@ void update_flags(uint16_t r);
 // 65536 locations
 uint16_t memory[UINT16_MAX];
 
+// registers
 enum
 {
     R_R0 = 0,
@@ -24,6 +36,7 @@ enum
 };
 uint16_t reg[R_COUNT];
 
+// operators
 enum
 {
     OP_BR = 0,
@@ -44,11 +57,30 @@ enum
     OP_TRAP
 };
 
+// flags
 enum
 {
     FL_POS = 1 << 0,
     FL_ZRO = 1 << 1,
     FL_NEG = 1 << 2,
+};
+
+// trap operations
+enum
+{
+    TRAP_GETC  = 0x20;
+    TRAP_OUT   = 0x21;
+    TRAP_PUTS  = 0x22;
+    TRAP_IN    = 0x23;
+    TRAP_PUTSP = 0x24;
+    TRAP_HALT  = 0x25;
+};
+
+// memory mapped registers
+enum
+{
+    MR_KBSR = 0xFE00; // keyboard status
+    MR_KBDR = 0xFE02; // keyboard data
 };
 
 int main(int argc, char** argv)
@@ -66,6 +98,9 @@ int main(int argc, char** argv)
             std::cout << "failed to load image: %s\n", argv[j]);
         }
     }
+
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 
     enum { PC_START = 0x3000 };
     reg[R_PC] = PC_START;
@@ -186,6 +221,45 @@ int main(int argc, char** argv)
             mem_write(reg[base] + offset, reg[sr]);
             break;
         case OP_TRAP:
+            switch (instr & 0xFF)
+            {
+            case TRAP_GETC:
+                char chr;
+                std::cin >> chr;
+                reg[R_R0] = (uint16_t) char;
+                break;
+            case TRAP_OUT:
+                char chr = reg[R_R0] & 0xFF;
+                std::cout << out;
+                break;
+            case TRAP_PUTS:
+                uint16_t* c = memory + reg[R_R0];
+                std::cout << (char*) c;
+                break;
+            case TRAP_IN:
+                char chr;
+                std::cout << "Enter a character: ";
+                std::cin >> chr;
+                std::cout << chr;
+                reg[R_R0] = (uint16_t) chr;
+                break;
+            case TRAP_PUTSP:
+                uint16_t* c = memory + reg[R_R0];
+                char chr;
+                while(*c)
+                {
+                    chr  = *c & 0xFF;
+                    std::cout << (char) chr;
+                    chr = *c >> 8;
+                    if (chr) std::cout << (char) chr;
+                    ++c;
+                }
+                break;
+            case TRAP_HALT:
+                std::cout << "HALT" << std::endl;
+                running = 0;
+                break;
+            }
             break;
         case OP_RES:
             break;
@@ -219,4 +293,95 @@ void update_flags(uint16_t r)
     {
         reg[R_COND] = FL_POS;
     }
+}
+
+void read_image(std::string path)
+{
+    std::ifstream file(path, std::ios::binary);
+    if (!file)
+    {
+        std::cout << "VM: Could not open file " << path << std::endl;
+    }
+    read_image_file(file);
+
+}
+
+void read_image_file(std::ifstream file)
+{
+    uint16_t origin;
+    file.read((char *) &origin, sizeof(origin));
+    origin = swap16(origin);
+
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t* p = memory + origin;
+    file.read((char *) p, max_read);
+    size_t read = file.gcount();
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+
+    }
+}
+
+uint16_t swap16(uint_t x)
+{
+    return (x << 8) | (x >> 8)
+}
+
+uint16_t mem_write(uint16_t address, uint16_t val)
+{
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = 1 << 15;
+            memory[MR_KBDR] = getchar();
+        }
+        else
+        {
+            memory[MR_KBSR] = 0;
+        }
+
+    }
+    return memory[address];
+}
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
 }
